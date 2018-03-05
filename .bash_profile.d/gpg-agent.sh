6cb6e5118e9d5770ecb217079ddc9083bb80e87a
Philip Dubois <hello@philipdubois.be>
2018-03-05 14:46:41 +0100

    GPG/SSH: Use ssh-agent instead of gpg-agent emulation

--- .bash_profile.d/gpg-agent.sh
+++ .bash_profile.d/gpg-agent.sh
@@ -4,9 +4,30 @@
 # https://wiki.archlinux.org/index.php/GnuPG#gpg-agent
 
 # Start the GPG agent
-# It's also used with ssh-agent emulation so don't forget to add your keys the
-# first time with ssh-add(1)
-unset SSH_AGENT_PID
 eval "$(gpg-agent --daemon 2> /dev/null)"
-GPG_TTY=$(tty)
-export GPG_TTY
+export GPG_TTY=$(tty)
+
+
+# === SSH-AGENT
+# https://help.github.com/articles/working-with-ssh-key-passphrases
+
+env=~/.ssh/agent.env
+
+agent_load_env () { test -f "$env" && . "$env" >| /dev/null ; }
+
+agent_start () {
+  (umask 077; ssh-agent >| "$env")
+  . "$env" >| /dev/null
+}
+
+agent_load_env
+
+# agent_run_state: 0=agent running w/ key; 1=agent w/o key; 2= agent not running
+agent_run_state=$(ssh-add -l >| /dev/null 2>&1; echo $?)
+
+if [ ! "$SSH_AUTH_SOCK" ] || [ $agent_run_state = 2 ]; then
+  agent_start
+fi
+
+unset env
+unset agent_run_state

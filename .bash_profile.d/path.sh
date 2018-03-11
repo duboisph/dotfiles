04aad071fa9d3bdf5d93d177b742e51c5348cca1
Philip Dubois <hello@philipdubois.be>
2018-03-11 18:28:09 +0100

    Bash: Add linuxbrew to PATH

--- .bash_profile.d/path.sh
+++ .bash_profile.d/path.sh
@@ -4,5 +4,12 @@
 
 PATH="$HOME/.gem/ruby/2.3.0/bin:$PATH"
 PATH="$HOME/.local/bin:$PATH"
-PATH="$HOME/.tfenv/bin:$PATH"
+#PATH="$HOME/.tfenv/bin:$PATH"
+
+PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+MANPATH="/home/linuxbrew/.linuxbrew/share/man:$MANPATH"
+INFOPATH="/home/linuxbrew/.linuxbrew/share/info:$INFOPATH"
+
 export PATH
+export MANPATH
+export INFOPATH

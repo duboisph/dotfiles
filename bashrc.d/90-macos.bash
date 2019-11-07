cf2d4bca7900031d8d2f13ec7957f377048b8987
Philip Dubois <hello@philipdubois.be>
2019-11-07 20:38:20 +0100

    Bash: Remove OS-specific workarounds
    - WSL workarounds are no longer needed in WSL2
    - I'm not really running macOS

--- bashrc.d/90-macos.bash
+++
@@ -1,6 +0,0 @@
-# === WSL SPECIFIC WORKAROUNDS
-if [[ "$(uname -s)" == "Darwin" ]]; then
-  # Set browser to Chrome
-  export DISPLAY=':0'
-  export BROWSER='/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-fi

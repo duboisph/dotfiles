0dc42f1eef20e63f395894da1b96ee89be02d3ee
Philip Dubois <hello@philipdubois.be>
2020-08-03 23:02:23 +0200

    Several updates

--- init/brew.sh
+++ init/brew.sh
@@ -6,27 +6,12 @@ BREW_PREFIX="$(brew --prefix)"
 brew update
 brew upgrade
 
-# Install latest Bash and set as default
-#brew install bash bash-completion@2
-
-#if ! grep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
-#  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells
-#  chsh -s "${BREW_PREFIX}/bin/bash"
-#fi
-
-# Install my must-haves
-#brew install \
-#  git \
-#  neovim \
-#  tmux
-
 # Install utilities
 brew install \
   ansible \
   awscli \
   docker-compose \
   git-crypt \
-  go \
   helm \
   jq \
   krew \
@@ -43,19 +28,28 @@ brew install \
   teleport \
   terraform \
   terraform-docs \
-  unzip \
   velero
 #  vault
 
 # Install these only on macOS (like newer version of core GNU utils)
 if [[ "$(uname -s)" == "Darwin" ]]; then
   brew install \
+    bash
+    bash-completion@2
     coreutils \
     findutils \
+    git \
     gnu-sed \
     grep \
     gnupg \
+    neovim \
     openssh \
     swig \
+    tmux \
     wget
+
+  # if ! grep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  #   echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells
+  #   chsh -s "${BREW_PREFIX}/bin/bash"
+  # fi
 fi

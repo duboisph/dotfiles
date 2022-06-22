165de8b3acfcb2b7bb5973b8358010640427eacf
Philip Dubois <hello@philipdubois.be>
2022-06-22 17:50:49 +0200

    Replace ls with exa. Update brew list

--- init/brew.sh
+++ init/brew.sh
@@ -5,26 +5,35 @@ brew update
 brew upgrade
 
 # Add some taps
+brew tap azure/kubelogin
 brew tap fluxcd/tap
+brew tap googlecontainertools/kpt
 brew tap skyscrapers/tap
 
 # Install utilities
 brew install \
   aws-vault \
+  exa \
   fluxctl \
   helm \
   istioctl \
   k9s \
+  kpt \
   krew \
   kubectx \
+  kubelogin \
   kubernetes-cli \
+  logcli \
+  neovim \
   sonobuoy \
   sops \
   starship \
   teleport \
   terraform \
   terraform-docs \
+  terragrunt \
   tf \
+  tmux \
   vault \
   velero \
   yq

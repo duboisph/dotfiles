c2353110574cee5570a2d5848002d22f38211a50
Philip Dubois <hello@philipdubois.be>
2025-05-20 11:39:51 +0200

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,16 @@
 # Set umask
 umask 0007
 
+# Prefer US English and use UTF-8
+set -gx LANG en_US.UTF-8
+set -gx LC_ALL en_US.UTF-8
+
+# Make nvim the default editor
+set -gx EDITOR nvim
+
+# Settings for GPG
+set -gx GPG_TTY (tty)
+
 # Load Homebrew
 set -gx HOMEBREW_NO_ENV_HINTS 1
 set -gx HOMEBREW_NO_ANALYTICS 1
@@ -18,7 +28,19 @@ if test (uname -s) = Darwin
 end
 fish_add_path --prepend --move $HOME/bin $HOME/.local/bin $HOME/.krew/bin/ $HOME/go/bin/
 
-# Commands to run in interactive sessions
+# Load completions
+if test -d (brew --prefix)"/share/fish/completions"
+    set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
+end
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end
+
+for file in ~/.config/fish/completions/*.fish
+    source $file
+end
+
+# Commands to run in interactive sessions only (like terminal)
 if status is-interactive
     # Remove greeting message
     set -g fish_greeting
@@ -26,31 +48,9 @@ if status is-interactive
     # Indicate 24-bit color support
     set -gx COLORTERM truecolor
 
-    # Prefer US English and use UTF-8
-    set -gx LANG en_US.UTF-8
-    set -gx LC_ALL en_US.UTF-8
-
-    # Make nvim the default editor
-    set -gx EDITOR nvim
-
-    # Settings for GPG
-    set -gx GPG_TTY (tty)
-
     # Load theme
     fish_config theme choose "Dracula Official"
 
     # Load Starship Prompt
     starship init fish | source
-
-    # Load completions
-    if test -d (brew --prefix)"/share/fish/completions"
-        set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
-    end
-    if test -d (brew --prefix)"/share/fish/vendor_completions.d"
-        set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
-    end
-
-    for file in ~/.config/fish/completions/*.fish
-        source $file
-    end
 end
